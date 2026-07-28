9745f64aade2a6f848af87f80d908598601db6df
Matome <Matmohohoma68@gmail.com>
2026-07-28 02:46:09 +0200

    lockTwo-Complete3

--- lockTwo.java
+++ lockTwo.java
@@ -1,40 +1,25 @@
-class lockTwo extends Thread implements lock  {
-    public static int victim;
-    // Implementing the locking algorithm
-    @Override
-    public void lock(int i) {
-        victim = i;
-        while(victim == i) {
-            System.out.println("Thread: " + i + " is waiting.");
-            System.out.println("victim = " + i + "\n");
-        }
-    }
-    
-    @Override
-    public void unlock(int i) {
-        System.out.println("Thread: " + i + " is out the critical section.");
+
+public class lockTwo extends Thread {
+    public static int count = 0;
+
+    public int i = 0;
+    public int id;
+    lockTwoTest lock = new lockTwoTest();
+
+    public lockTwo() {
+        id=count++;
     }
 
-    // Main for testing the lockOne lock algorithm
-    public static void main(String[] args) {
-        switch (args[0]){
-            case "1": {
-                lockTwoConcurrent thread0 = new lockTwoConcurrent();
-                lockTwoConcurrent thread1 = new lockTwoConcurrent();
-
-                thread0.start();
-                thread1.start();
-                break; 
-            }  
-            case "2": {
-                lockTwoSequential thread0 = new lockTwoSequential();
-                lockTwoSequential thread1 = new lockTwoSequential();
-
-                thread0.run();
-                thread1.run();
-                break; 
-            }     
-            default: return;       
+    @Override
+    public void run() {
+        lock.lock(this.id);
+        try {
+            int shared = 0;
+            shared = shared + 1;
+            shared = shared + 1;
+            System.out.print("Thread: " + this.id + " is in the critical section.\n");
+        } finally {
+            lock.unlock(this.id);
         }
     }
 }
